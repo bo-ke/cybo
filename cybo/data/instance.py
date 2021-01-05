05a7ff69188b6a7541db1a1b6ccf259f7a833f18
kebo <itachi971009@gmail.com>
2021-01-06 00:50:10 +0800

    [fea] 新增instance基类

--- cybo/data/instance.py
+++ cybo/data/instance.py
@@ -11,3 +11,25 @@
 
 
 '''
+from typing import Mapping, MutableMapping, Dict
+
+from cybo.data.fields.field import Field
+
+class Instance(Mapping[str, Field]):
+
+    def __init__(self, fields: MutableMapping[str, Field]) -> None:
+        self.fields = fields
+        # indexed flag
+        self.indexed = False
+    
+    def count_vocab_items(self, counter: Dict[str, Dict[str, int]]):
+        """在instance级别，进行count_vocab操作，计算counter，用于生成vocab
+            在vocabulary的from_instances方法中被调用
+
+        Args:
+            counter (Dict[str, Dict[str, int]]): vocab->counter
+        """
+        for field in self.fields.values():
+            # 调用每一个field的count_vocab_items函数
+            field.count_vocab_items(counter)
+        
