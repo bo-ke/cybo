2a1c227f829399cd89648595f198eab2d27a2bf7
kebo <itachi971009@gmail.com>
2021-03-02 23:29:42 +0800

    [fea] add common logger

--- cybo/common/logger.py
+++ cybo/common/logger.py
@@ -4,13 +4,14 @@
 @contact: kebo0912@outlook.com
 
 @version: 1.0
-@file: __init__.py
-@time: 2020/12/16 23:19:09
+@file: logger.py
+@time: 2021/03/02 22:40:25
 
 这一行开始写关于本文件的说明与解释
 
 
 '''
+from loguru import logger
 
 
 def func():
