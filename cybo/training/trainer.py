9441e768dce9f20d1dd261d16cf97cf50c02f1c4
kebo <itachi971009@gmail.com>
2021-04-18 18:16:43 +0800

    [fea] add training early stop

--- cybo/training/trainer.py
+++ cybo/training/trainer.py
@@ -23,8 +23,8 @@ class Trainer():
                  epochs: int,
                  checkpoint_path: str,
                  validation_dataloader: Dataloader = None,
-                 patience: int = None,
-                 save_weights_only: bool = True,
+                 patience: int = 5,
+                 max_to_keep: int = 3,
                  monitor: str = "overall_acc"
                  ) -> None:
 
@@ -39,18 +39,21 @@ class Trainer():
 
         # self.output_dir = output_dir
         self.checkpoint_path = checkpoint_path
+        self.max_to_keep = max_to_keep
         self.monitor = monitor
+        self.patience = patience
 
     def train(self):
         ckpt = tf.train.Checkpoint(
             model=self.model, optimizer=self.optimizer, epoch=tf.Variable(1))
         ckpt_manager = tf.train.CheckpointManager(
-            ckpt, self.checkpoint_path, max_to_keep=5)
+            ckpt, self.checkpoint_path, max_to_keep=self.max_to_keep)
         if ckpt_manager.latest_checkpoint:
             ckpt.restore(ckpt_manager.latest_checkpoint)
             tf.print("restore from latest checkpoint succeed !")
 
         best_acc = 0.0
+        early_stop_epochs = 0
         for epoch in tf.range(ckpt.epoch, self.epochs+1):
             tf.print(f"Epoch {epoch}/{self.epochs}:")
             # 更新ckpt中epoch值
@@ -79,8 +82,13 @@ class Trainer():
                 tf.print(
                     f"Saving checkpoint for epoch {epoch} at {ckpt_save_path}")
                 best_acc = evaluate_metrics.get(self.monitor, 1.0)
+                early_stop_epochs = 0
             else:
                 tf.print(f"validation {self.monitor} is not improved")
+                early_stop_epochs += 1
+            if early_stop_epochs >= self.patience:
+                tf.print(f"Early stopping with patience {self.patience}")
+        tf.print("Training completed !")
 
     # @tf.function()
     def train_step(self, batch):
